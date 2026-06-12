35495ba5796e5815fc1e429bf16dc547fdd26eb0
PriyangaSelvaraju1994 <priyanga.selvaraju@apexon.com>
2026-06-12 12:51:12 +0530

    get all leaves endpoint added and authorized all endpoints

--- Backend/HRConnect.API/Services/Implementations/JwtService.cs
+++ Backend/HRConnect.API/Services/Implementations/JwtService.cs
@@ -12,11 +12,15 @@ public class JwtService : IJwtService
         _configuration = configuration;
     }
 
-    public string GenerateToken(string email)
+    public string GenerateToken(User user)
     {
         var claims = new[]
         {
-            new Claim(ClaimTypes.Email, email)
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.FullName),
+            new Claim(
+            ClaimTypes.Role,
+            user.IsAdmin ? "Admin" : "Employee")
         };
 
         var key = new SymmetricSecurityKey(
